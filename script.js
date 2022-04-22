7c2385013fd87702e521a693e313a5fe9e62d17c
Oscar <oscnor0930@skola.goteborg.se>
2022-04-22 11:43:07 +0200

    print message to console.log and index.html in JSON

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ const AREA_URL = "http://api.sr.se/api/v2/traffic/areas"
 "http://api.sr.se/api/v2/traffic/areas?format=json&page=2"
 
 
-//funkar ej
-// async function getArea() {
-//     const endpoint = `${AREA_URL}?format=json`
-//     const response = await fetch(endpoint)
-//     const results = response.json()
-//     let resultpage = document.getElementById('areas')
-//     resultpage.innerText = results.resultpage
-//     resultpage.style.visibility = 'visible'
-// }
-// //Button funkar
 let areaButton = document.getElementById('areaButton');
 areaButton.addEventListener('click', () => addAreaToElement())
 
@@ -39,9 +29,9 @@ async function fetchJson() {
     const response = await fetch('http://api.sr.se/api/v2/traffic/areas?format=json&page=2')
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            //console.log(data)
             const jsonToString = JSON.stringify(data.areas.map(areas => areas.name))
-            console.log('jsonToString', jsonToString)
+                //console.log('jsonToString', jsonToString)
 
         })
     if (!response.ok) {
@@ -50,21 +40,51 @@ async function fetchJson() {
     return console.log(data.areas)
 }
 
-async function fetchMessege() {
-    const response = await fetch('http://api.sr.se/api/v2/traffic/messages?format=json&page=15')
-        .then(res => res.json())
-        .then(data => {
-            let message = document.getElementById('test')
-            let areaInfo = document.createElement('pre')
-            areaInfo.innerText = JSON.stringify(data)
-            message.appendChild(areaInfo)
-            console.log(data)
-            const messagesToString = JSON.stringify(data)
-            console.log(messagesToString)
+// async function fetchMessege() {
+//     const response = await fetch('http://api.sr.se/api/v2/traffic/messages?format=json&page=2&size=15')
+//         .then(res => res.json())
+//         .then(data => {
+//             let message = document.getElementById('test')
+//             let areaInfo = document.createElement('pre')
+//                 console.log(data)
+//             let arr = Object.entries(data.messages)
+//                 console.log(arr)
 
-        })
-    if (!response.ok) {
-        throw new Error("response error")
-    }
-    // return console.log(data.areas)
+//             areaInfo.innerText = JSON.stringify(data)
+//             message.appendChild(areaInfo)
+//                 console.log(data)
+//             const messagesToString = JSON.stringify(data)
+//                 console.log(messagesToString)
+
+//         })
+//     if (!response.ok) {
+//         throw new Error("response error")
+//     }
+//      return console.log(data.areas)
+// }
+
+async function getMesseges() {
+    const response = await fetch('http://api.sr.se/api/v2/traffic/messages?format=json&page=2&size=15')
+    const data = await response.json()
+        //console.log(data)
+
+    let parent = document.getElementById('test')
+    data.messages.forEach(message => printMessage(parent, message))
+
 }
+
+function printMessage(parent, message) {
+    // const { id, priority, createdate, title, exactlocation, description, latitude, longitude, category, subcategory } = data
+    const messageElement = document.createElement('div')
+    messageElement.className = "messageItem"
+    messageElement.innerText = JSON.stringify(message)
+        //messageElement.innerText = message.description
+
+    //console.log(message)
+    for (i in message) {
+        console.log(i + "-" + (message[i])) //printar till console.log
+    }
+    //printar till index.html
+    parent.appendChild(messageElement)
+}
+getMesseges()
