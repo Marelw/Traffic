d535ee04bb12e23c43265693e3d1d3729f4aadda
Oscar <oscnor0930@skola.goteborg.se>
2022-04-27 16:22:42 +0200

    Vue component test

---
+++ JavaxUX/src/App.vue
@@ -0,0 +1,89 @@
+<script setup>
+import Areas from "./components/AreaButton.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+//import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <Areas />
+
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+
+.messageContainer {
+  border: 1px solid black;
+  margin: 2px;
+  display: grid;
+  grid-template-columns: 100px 1fr 1fr 1fr;
+}
+
+.messageItem {
+  color: black;
+  background-color: hotpink;
+}
+/* @import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+} */
+</style>
