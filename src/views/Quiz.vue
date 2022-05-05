3a560f7821eb14ef776aad886aa77bf048ec4f3e
Oscar <oscnor0930@skola.goteborg.se>
2022-05-05 11:13:22 +0200

    timer

---
+++ src/views/Quiz.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="dropdown">
+            <button class="dropbtn">Choose Zone</button>
+            <div id="dropdown-content-id" class="dropdown-content"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+
+</script>
+
+
+<style>
+
+
+</style>
