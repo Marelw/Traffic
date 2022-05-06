534b08e037877ef48181385daf13ec7bd2359729
Oscar <oscnor0930@skola.goteborg.se>
2022-05-06 13:42:20 +0200

    asld

--- src/views/TrafficView.vue
+++ src/views/TrafficView.vue
@@ -1,9 +1,40 @@
 <template>
+    <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">kategori + ikon </h5>
+            <p class="card-text">
+                Marcus har kört i diket.
+            </p>
+        </div>
+    </div>
+        <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">kategori + ikon </h5>
+            <p class="card-text">
+                Marcus har kört i diket.
+            </p>
+        </div>
+    </div>
+        <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">kategori + ikon </h5>
+            <p class="card-text">
+                Marcus har kört i diket.
+            </p>
+        </div>
+    </div>
+        <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <h5 class="card-title">kategori + ikon </h5>
+            <p class="card-text">
+                Marcus har kört i diket.
+            </p>
+        </div>
+    </div>
     
+
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
