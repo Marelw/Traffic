912676197af8c82b7e3191c1c0ea5582228d21a6
Oscar <oscnor0930@skola.goteborg.se>
2022-05-06 11:37:38 +0200

    navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,14 +1,36 @@
 <template>
-    <router-link to="/">
-        <button>Hem</button>
-    </router-link>
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-light p-4">
+            <router-link to="/">
+                <button>Hem</button>
+            </router-link>
 
-    <router-link to="/quizview">
-        <button>Quiz</button>
-    </router-link>
+            <router-link to="/quizview">
+                <button>Quiz</button>
+            </router-link>
 
-    <router-link to="/trafficview">
-        <button>Trafikinformation</button>
-    </router-link>
+            <router-link to="/trafficview">
+                <button>Trafikinformation</button>
+            </router-link>
+        </div>
+    </div>
+    <nav class="navbar navbar-light" style="background-color: $orange-200">
+        <div class="container-fluid">
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarToggleExternalContent"
+                aria-controls="navbarToggleExternalContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+
+    <button type="button" class="btn btn-primary">Primary</button>
+
 
 </template>
