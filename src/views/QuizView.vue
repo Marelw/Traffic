7fcb111e4524a86eb39ab4107144bc553a285ed5
Oscar <oscnor0930@skola.goteborg.se>
2022-05-06 13:28:25 +0200

    quiz

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,3 +1,22 @@
 <template>
-    
+    <div class="d-block bg-secondary">
+        <div>hejhej</div>
+    </div>
+    <div class="card text-center border-dark border-2">
+        <div class="card-header">Quiz</div>
+        <div class="card-body">
+            <h5 class="card-title">fråga</h5>
+            <p class="card-text">placeholder för fråga</p>
+            <button class="btn btn-secondary btn-lg my-2 col-12" type="button">spela upp fråga igen</button>
+        </div>
+    </div>
+    <div class="">
+        <button class="btn btn-secondary btn-lg my-2 col-12 w-50 p-3" type="button" style="width: 20%">
+            Starta/Avsluta
+        </button>
+    </div>
 </template>
+
+<script>
+export default {}
+</script>
