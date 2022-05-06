3f99bc5e408cc590ef2289f1549eef48298c33fd
Oscar <oscnor0930@skola.goteborg.se>
2022-05-06 11:20:33 +0200

    start

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <router-link to="/">
+        <button>Hem</button>
+    </router-link>
+
+    <router-link to="/quizview">
+        <button>Quiz</button>
+    </router-link>
+
+    <router-link to="/trafficview">
+        <button>Trafikinformation</button>
+    </router-link>
+
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import QuizView from "@/views/QuizView.vue";
+import TrafficView from "@/views/TrafficView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+    history: createWebHistory(),
+    routes: [
+        { path: "/", name: "HomeView", component: HomeView },
+        { path: "/quizview", name: "QuizView", component: QuizView },
+        { path: "/trafficview", name: "TrafficView", component: TrafficView },
+    ],
+})
 
-  ],
-});
 
 export default router;
