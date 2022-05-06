c625643592213e20b76b7deb5eabc477d80e7fd2
Oscar <oscnor0930@skola.goteborg.se>
2022-05-06 12:42:45 +0200

    button size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <div class="d-grid gap-2 col-6 mx-auto justify-content-md-center">
+        <div class="d-grid gap-2 justify-content-md-center">
             <div>
-                <router-link to="/quizview">
-                    <button type="button" class="btn btn-secondary btn-lg btn-block my-2">Quiz</button>
-                </router-link>
             </div>
             <div >
-                <router-link to="/trafficview">
-                    <button type="button" class="btn btn-secondary btn-lg btn-block my-2">Trafikinformation</button>
+            </div>
+
+            <div class="d-grid gap-2">
+                <router-link to="/quizview">
+                    <button class="btn btn-secondary btn-lg my-2" type="button" style="width: 100%">Quiz</button>
                 </router-link>
+                <router-link to="/trafficview">
+                    <button class="btn btn-secondary btn-lg my-2" type="button" style="width: 100%">Trafikinformation</button>
+                </router-link>
+            
             </div>
 
         </div>
