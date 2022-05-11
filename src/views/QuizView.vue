925d0aea5cdb6b6b1727a44f6e1e8558060c5d19
Oscar <oscnor0930@skola.goteborg.se>
2022-05-11 15:21:48 +0200

    quiz

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,27 +1,38 @@
 <template>
     <div class="card text-center border-dark border-2">
-        <div class="card-header">Quiz</div>
+        <div class="card-header">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">{{ dropdownTitle }}</button>
+                        <div class="dropdown-menu" style="max-height: 11rem; overflow-y: auto" >
+                            <div v-for="area in trafficZones" ><a class="dropdown-item" @click="chooseZone" >{{ area.name }} </a> 
+                        </div>
+                </div>
+            </div>
+            <!-- <button type="button" class="btn btn-secondary btn-sm btn-block mx-2" @click="locate">Min plats</button> -->
+            <p v-if="statusMessage !== ''">{{ statusMessage }}</p>
+            <div class="">
+                <button class="btn btn-secondary btn-sm btn-block mx-2" type="button" style="width: 20%" @Click="play">Start</button>
+                <button class="btn btn-secondary btn-sm btn-block mx-2" type="button" style="width: 20%" @Click="pause">Pausa</button>
+            </div>
+        </div>
         <div class="card-body">
             <h5 class="card-title">fråga</h5>
-            <p class="card-text">placeholder för fråga</p>
+            <p class="card-text">{{ quizQuestion }}</p>
+            <!-- v-if="quizAnswer !== ''" -->
+            <h6 class="card-title">Svar:</h6>
+            <p class="card-title">{{ quizAnswer }}</p>
             <button class="btn btn-secondary btn-lg my-2 col-12" type="button">spela upp fråga igen</button>
         </div>
-    </div>
-    <div class="">
-        <button class="btn btn-secondary btn-lg my-2 col-12 w-50 p-3" type="button" style="width: 20%">
-            Starta/Avsluta
-        </button>
-    </div>
-
-    <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Choose Zone</button>
-        <div class="dropdown-menu" style="max-height: 11rem; overflow-y: auto" >
-            <div v-for="area in trafficZones" ><a class="dropdown-item" @click="chooseZone" >{{ area.name }} </a></div>
+        <div>
+            <p>Nästa fråga ges om: {{ timerCount}} sec</p>
         </div>
     </div>
+    
 </template>
 
 <script>
+const AREA_URL = "http://api.sr.se/api/v2/traffic/areas"
+
 export default {
     data () {
         return {
@@ -32,14 +43,67 @@ export default {
                 ],
                 trafficZones: [],
                 yourLocation: "",
+                timerCount: 10,
+                timerEnabled: false,
+                dropdownTitle: "Örebro",
+
+                quizQuestion: "",
+                quizAnswer: "",
+            
         }
     },
+    watch: {
+
+        timerEnabled(value) {
+            if (value) {
+                setTimeout(() => {
+                    this.timerCount--;
+                }, 1000);
+            }
+        },
+
+        timerCount: {
+            handler(value) {
+
+                if (value > 0 && this.timerEnabled) {
+                    setTimeout(() => {
+                        this.timerCount--;
+
+                    }, 1000);
+                }
+
+            },
+            immediate: true, // Välj om watch ska triggas när sidan skapas
+        }
+    },
+
+
     mounted() {
         // this.locate()
         // this.getMessages(this.yourLocation)
         this.dropdownAreas()
+        this.locate()
     },
+    created() {
+        //this.countDownTimer()
+         //this.pause()
+    },
+    
     methods: {
+            play() {
+                if (this.dropdownTitle === "Göteborg") {
+                    this.quizGBG()
+                } else if (this.dropdownTitle === "Örebro") {
+                    console.log("Frågor för Örebro kommer inom kort...")
+                } else {
+                    console.log("Detta område stöds ej ännu. Donera till CarPlay på Swish: 0720468084")
+                }
+                this.timerEnabled = true;
+            },
+
+            pause() {
+                this.timerEnabled = false;
+            },
         async locate() {
 
             const findMe = async (position) => {
@@ -68,36 +132,45 @@ export default {
 
             data.areas.forEach(area => this.trafficZones.push(area))
         },
+        chooseZone(event) {
+            
+            let element = event.target.innerText
+            this.dropdownTitle = element
+
+        },
         async getTrafficAreaTest(latitude, longitude) {
             const response = await fetch(`${ AREA_URL }?format=json&latitude=${ latitude }&longitude=${ longitude }`)
             const data = await response.json()
 
             this.yourLocation = data.area.name
+            this.dropdownTitle = this.yourLocation
             
             // IF statement för örebro och gbg
-            this.quizGBG(this.yourLocation)
+            //this.quizGBG(this.yourLocation)
         },
 
-       async quizGBG(areaName) {
+       async quizGBG() {
         const delay = ms => new Promise(res => setTimeout(res, ms))
 
-         if (areaName === "Göteborg") {
            let shuffledArray = this.gbgQuizArray.sort(() => 0.5 - Math.random())
-           console.log(areaName)
+           
            
             for (let i = 0; i <= shuffledArray.length + 1; ++i) {
               if (shuffledArray.length === 0) {
                 console.log("slut")
               } else {
                 let firstElement = shuffledArray.shift()
-                  await delay(2000);
-                  console.log(firstElement.question)
+                  await delay(10000);
+                  this.timerCount = 10
+                  //console.log(firstElement.question)
+                  this.quizQuestion = firstElement.question
       
-                  await delay(2000);
-                  console.log(firstElement.answer);
+                  await delay(5000);
+                  this.timerCount = 10
+                  //console.log(firstElement.answer);
+                  this.quizAnswer = firstElement.answer
               }
             }
-         }
        },
 
     }
