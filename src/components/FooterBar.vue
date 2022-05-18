73650ce670da3290e043cc1f32a219aceec136d8
HamodiBadljvi <mohbad0315@skola.goteborg.se>
2022-05-18 12:49:52 +0200

    better fotterBar

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,10 +1,28 @@
 <template>
     <footer class="position-sticky mt-auto py-3 bg-secondary container-fluid" >
     <div class="container" >
-        <div>Quiz</div>
-        <div>Trafikinfo</div>
-        <div>Om oss</div>
-        <span class="text-muted">Place sticky footer content here.</span>
+        <!-- <div><a class="quizView">Quiz</a></div>
+        <div><a class="trafficView">Trafikinfo</a></div>
+        <div><a class="aboutView">Om oss</a></div>
+        <span class="text-muted">Place sticky footer content here.</span> -->
+
+            <router-link to="/trafficview">
+                <a class="trafficView">
+                    <div>Trafikinfo</div>
+                </a>
+            </router-link>
+
+            <router-link to="/quizview">
+                <a class="quizView">
+                    <div>Quiz</div>
+                </a>
+            </router-link>
+
+            <router-link to="/aboutview">
+                <a class="aboutView">
+                    <div>Om oss</div>
+                </a>
+            </router-link>
     </div>
 </footer>
 </template>
